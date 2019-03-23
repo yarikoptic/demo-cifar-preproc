5cc4d4e732ec095a4bdb4bca453fc56d27726d92
Yaroslav Halchenko <debian@onerussian.com>
2019-03-23 10:55:08 -0400

    ENH: Make paths and batch size configurable

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,5 +1,8 @@
 # pip install torch torchvision xarray netCDF4
 
+import os
+import sys
+import os.path as op
 from math import ceil
 from sys import stdout
 
@@ -14,18 +17,24 @@ def print_progress(progression_str, current, end):
     stdout.write(progression_str.format(current, end))
     stdout.flush()
 
-batch_size = 256
+if len(sys.argv) < 3:
+    sys.stderr.write("Please provide input_path output_path [batch_size]")
+    sys.exit(1)
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+batch_size = int(sys.argv[3]) if len(sys.argv) > 3 else 256
 
 transform = transforms.Compose([
     lambda img: np.array(img)
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data/rawdata/', train=True, # download=True,
+trainset = torchvision.datasets.CIFAR10(root=input_path, train=True, # download=True,
                                         transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=False, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='./data/rawdata/', train=False, # download=True,
+testset = torchvision.datasets.CIFAR10(root=input_path, train=False, # download=True,
                                        transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
@@ -51,7 +60,10 @@ for batch_idx, (inputs, targets) in enumerate(trainloader):
 
 train_dataset = xr.Dataset()
 train_dataset['data'] = train_container
-train_dataset.to_netcdf('./data/train.h5')
+
+if not op.exists(output_path):
+    os.makedirs(output_path)
+train_dataset.to_netcdf(f'{output_path}/train-{batch_size}.h5')
 
 progression_str = 'Creating test dataset: batch {:d}/{:d}'
 stdout.write('\n'+progression_str.format(0, test_batch_cnt))
@@ -70,4 +82,4 @@ stdout.flush()
 
 test_dataset = xr.Dataset()
 test_dataset['data'] = test_container
-test_dataset.to_netcdf('./data/preproc/test.h5')
+test_dataset.to_netcdf(f'{output_path}/test-{batch_size}.h5')
